d4d71d356bf5e78fe39103a7594d51b503889aed
Shubhanshu Chouhan <s4h1u1b9h9a2m@gmail.com>
2019-11-28 17:45:27 +0530

    Coping with the speed of user & updated text width calculation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,22 +15,48 @@ export default {
   data() {
     return {
       text: "",
-      factor: 7.7
+      factor: 7.7,
+      constantSideWidth: 20,
+      canvas: document.createElement("canvas"),
+      font: null,
+      typingSpeed: 20
     };
   },
   watch: {
-    text(val) {
+    text(newVal, oldVal) {
+      this.typingSpeed = (this.getTextLength(newVal, this.font) - this.getTextLength(oldVal, this.font))
       this.resize();
     }
+  },
+  mounted () {
+    this.font = getComputedStyle(this.$refs["rp-input"])['font']
+    console.log('font', this.font)
   },
   methods: {
     resize() {
       let el = this.$refs["rp-input"];
-      const newWidth = (this.text.length + 1) * this.factor;
-      // if (newWidth > 50)
+      // const newWidth = (this.text.length + 1) * this.factor;
+      let newWidth = this.getTextLength(this.text, this.font) + this.constantSideWidth;
+      if (this.typingSpeed > 7.8) {
+        newWidth += this.typingSpeed * 10.5
+      }
       el.style.width = newWidth + "px";
-      console.log(this.$refs["rp-input"]);
+      console.log('typing speed', this.typingSpeed)
+      this.typingSpeed = 0
+      // console.log(this.$refs["rp-input"]);
+      // console.log(this.$refs["rp-input"].style['font'])
+      // console.log(getComputedStyle(this.$refs["rp-input"])['font'])
+      // let font = getComputedStyle(this.$refs["rp-input"])['font']
+      // console.log(this.getTextLength(this.text, this.font))
+    },
+    getTextLength(text, font) {
+      let context = this.canvas.getContext("2d");
+      context.font = font;
+      const metrics = context.measureText(text);
+      return metrics.width;
     }
+
+// console.log(getTextWidth("hello there!", "bold 12pt arial"));
   }
 };
 </script>
